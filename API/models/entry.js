e99aba56761274486366003229f1da6fe802ffa4
Niisoks <colebrhall@gmail.com>
2023-05-31 22:20:00 +0100

    Added mongoose models for user and todo entry

---
+++ API/models/entry.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const entrySchema = new Schema({
+    text : {
+        type: String,
+        required : true,
+        default : ""
+    },
+    complete : {
+        type: Boolean,
+        default: false
+    },
+    timestamp : {
+        type: Date,
+        default: Date.now()
+    },
+    user : {
+        type: mongoose.Types.ObjectId, 
+        ref: 'User', 
+        default: ""
+    }
+})
+
+const Entry = mongoose.model();
--- API/server.js
+++ API/server.js
@@ -5,6 +5,14 @@ const url = require('./config.js')
 
 const app = express();
 
-console.log(url);
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(url ,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("Connected to " + url))
+    .catch(console.error);
+
+app.listen(3001, () => console.log("Server started on port 3001"));
