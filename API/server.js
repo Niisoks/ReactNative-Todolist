2ffe8859647edee2e5f5e65bf57cac54f43543f5
Niisoks <colebrhall@gmail.com>
2023-06-05 01:42:57 +0100

    Update server.js

--- API/server.js
+++ API/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const url = require('./config.js')
+const url = require('./config.js');
 
 const app = express();
 
